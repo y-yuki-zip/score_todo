de31d0384b2353778c7d59c5b117be75a0532260
y-yuki-zip <yuki.104.bis@gmail.com>
2023-05-17 00:08:35 +0900

    fix タブの動的作成

--- lib/todo_list/todo_group.dart
+++ lib/todo_list/todo_group.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:score_todo/todo_list/todo_list.dart';
+import 'package:score_todo/todo_list/todo_value_object.dart';
 
 /// Todoリスト一覧画面をまとめたクラス
 class TodoGroup extends StatefulWidget {
@@ -12,66 +13,85 @@ class TodoGroup extends StatefulWidget {
 }
 
 /// Todoリスト一覧画面の状態
-class TodoGroupState extends State<TodoGroup> {
-  // タブのヘッダー
-  static const List<Widget> tabs = [
-    Tab(
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-        child: Text('買い物リスト'),
-      ),
-    ),
-    Tab(
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-        child: Text('その他'),
-      ),
-    ),
-    Tab(
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-        child: Text('List'),
-      ),
-    ),
-    Tab(
-      child: Padding(
-        padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-        child: Text('果物リスト'),
-      ),
-    ),
-    Tab(
-        child: Padding(
-            padding: EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
-            child: Icon(Icons.add_circle)))
+class TodoGroupState extends State<TodoGroup> with TickerProviderStateMixin {
+  // デフォルトタブ名
+  static const _defaultTabName = '新規リスト';
+  // タブコントローラー
+  TabController? _tabController;
+  // Todoリストのタスクを表す
+  List<TodoValueObject> _tabValues = [
+    TodoValueObject(_defaultTabName, 0),
+    TodoValueObject(_defaultTabName, 1)
   ];
 
-  // タブのボディ
-  static const List<Widget> tabBodies = [
-    TodoListPage(),
-    TodoListPage(),
-    TodoListPage(),
-    TodoListPage(),
-  ];
+  /// Tabを追加する
+  void _addTab() {
+    setState(() {
+      _tabValues.add(TodoValueObject(_defaultTabName, _tabValues.length));
+      _tabController = _createTabController();
+    });
+  }
+
+  /// Tabを削除する
+  /// [priority]消す対象の順番
+  void _removeTab(int priority) {
+    setState(() {
+      _tabValues.removeAt(priority);
+      _tabController = _createTabController();
+    });
+  }
+
+  /// TabControllerを作成する
+  TabController _createTabController() {
+    return TabController(length: _tabValues.length, vsync: this);
+  }
+
+  /// 表示する際に呼び出される
+  @override
+  void initState() {
+    super.initState();
+    _tabController = _createTabController();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: DefaultTabController(
-        length: tabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-              title: const Text('ToDo'),
-              bottom: const PreferredSize(
-                  // タイトルとの余白を調整できる
-                  preferredSize: Size.fromHeight(20),
-                  // タブバー
-                  child: TabBar(
-                    isScrollable: true,
-                    tabs: tabs,
-                  ))),
-          body: const TabBarView(
-            children: tabBodies,
-          ),
+    return DefaultTabController(
+      length: _tabValues.length,
+      child: Scaffold(
+        appBar: AppBar(
+            title: const Text('ToDo'),
+            actions: [
+              // 追加ボタン
+              IconButton(
+                onPressed: () {
+                  _addTab();
+                },
+                icon: const Icon(Icons.add),
+              ),
+              // メニューボタン
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_horiz),
+              )
+            ],
+            bottom: PreferredSize(
+                // タイトルとの余白を調整できる
+                preferredSize: const Size.fromHeight(20),
+                // タブバー
+                child: TabBar(
+                  isScrollable: true,
+                  controller: _tabController,
+                  tabs: _tabValues
+                      .map((e) => Tab(
+                              child: Padding(
+                            padding:
+                                const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 5.0),
+                            child: Text(e.name),
+                          )))
+                      .toList(),
+                ))),
+        body: TabBarView(
+          children: _tabValues.map((e) => TodoListPage()).toList(),
         ),
       ),
     );
--- lib/todo_list/todo_value_object.dart
+++ lib/todo_list/todo_value_object.dart
@@ -4,7 +4,9 @@ class TodoValueObject {
   int id = 0;
   // リスト名(name)
   String name = '';
+  // リスト名(name)
+  int priority = 0;
 
   // イニシャライザ
-  TodoValueObject(this.id, this.name);
+  TodoValueObject(this.name, this.priority);
 }
